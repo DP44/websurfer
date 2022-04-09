b94fb23169aa6382e2ac562df7d4492e85876743
KuteKetX <KuteKetBhop@gmail.com>
2022-04-08 19:29:15 -0600

    slight change to logging format

--- websurf/logger.py
+++ websurf/logger.py
@@ -5,21 +5,21 @@ debug_mode = False
 
 def error(msg):
     print(f"\x1B[90m[\x1B[93m{name}\x1B[90m] " + 
-        f"[\x1B[31m-\x1B[90m]\x1B[0m {msg}")
+        f"[\x1B[31m - \x1B[90m]\x1B[0m {msg}")
 
 def success(msg):
     print(f"\x1B[90m[\x1B[93m{name}\x1B[90m] " + 
-        f"[\x1B[32m+\x1B[90m]\x1B[0m {msg}")
+        f"[\x1B[32m + \x1B[90m]\x1B[0m {msg}")
 
 def message(msg):
     print(f"\x1B[90m[\x1B[93m{name}\x1B[90m] " + 
-        f"[\x1B[34m*\x1B[90m]\x1B[0m {msg}")
+        f"[\x1B[34m * \x1B[90m]\x1B[0m {msg}")
 
 def warning(msg):
     print(f"\x1B[90m[\x1B[93m{name}\x1B[90m] " + 
-        f"[\x1B[33m!\x1B[90m]\x1B[0m {msg}")
+        f"[\x1B[33m ! \x1B[90m]\x1B[0m {msg}")
 
 def debug(msg):
     if debug_mode:
         print(f"\x1B[90m[\x1B[93m{name}\x1B[90m] " + 
-            f"[\x1B[33m* DEBUG\x1B[90m]\x1B[0m {msg}")
+            f"[\x1B[33mDBG\x1B[90m]\x1B[0m {msg}")
